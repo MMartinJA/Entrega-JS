b0507c27e356725dcda07e9907bcac41321a980a
MMartinJA <moschinimartin8@gmail.com>
2023-11-04 16:27:15 -0300

    entrega 3 Ejercicios JS

--- entrega/ejercicio06.js
+++ entrega/ejercicio06.js
@@ -1,2 +1,6 @@
-//Ejercicio 5 
-//Programar un contador de caracteres para un textarea.
+const texto = document.getElementById('texto');
+const contar = document.getElementById('contar');
+
+texto.addEventListener('keyup', function(){
+    contar.innerText = texto.value.length;
+})
